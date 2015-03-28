f7d85af5e2ad51e3df9053eb06be306734899689
Philip Shurpik <Philip.Shurpik@KruscheCompany.com>
2015-03-29 01:27:42 +0200

    Added calculator to console

--- app/bot.js
+++ app/bot.js
@@ -25,18 +25,49 @@ Bot.prototype.joinRoom = function(roomName) {
     this.gitter.rooms.join(roomName)
         .then(function(room) {
             console.log('Joined room: ', room.name);
-            this.listenToMessages(room);
+            var events = room.listen();
+            events.on('message', this.onNewMessage.bind(this));
         }.bind(this))
         .fail(function(err) {
             console.log('Error. Not possible to join the room: ' + roomName + "\n", err);
         });
 };
 
-Bot.prototype.listenToMessages = function(room) {
-    var events = room.listen();
-    events.on('message', function(message) {
-        console.log('New message:', message.text);
-    });
+Bot.prototype.onNewMessage = function(message) {
+    var expression = message.text;
+    var errorString = utils.checkExpression(expression);
+    if (errorString.length > 0) {
+        console.log(errorString);
+        return;
+    }
+    expression = utils.trim(expression);
+    try {
+        var result = math.eval(expression);
+        console.log("calc " + expression + " = " + result);
+    }
+    catch (exc) {
+        console.log(exc);
+    }
+};
+
+var utils = {
+    trim: function(expression) {
+        return expression.substr(expression.indexOf('calc') + 4).trim();
+    },
+    checkExpression: function(expression) {
+        var regexp = new RegExp(/^[0-9+*().\-\/]+$/);
+        if (expression.indexOf('calc') === -1) {
+            return "Error! No 'calc' keyword in message!";
+        }
+        expression = utils.trim(expression);
+        if (expression.length === 0) {
+            return 'Error! Nothing to evaluate';
+        }
+        if (!regexp.test(expression)) {
+            return 'Error! Invalid characters in expression';
+        }
+        return "";
+    }
 };
 
 module.exports = new Bot();
